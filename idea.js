f29a637df472c029dfbb9221a96544a56e14dd09
SiimonStark <justinpyktel@gmail.com>
2019-05-16 20:25:47 -0600

    Create Idea Class

---
+++ idea.js
@@ -0,0 +1,22 @@
+class Idea {
+  constructor(id, title, body, quality){
+    this.id = id,
+    this.title = title,
+    this.body = body,
+    this.quality = quality
+  }
+
+  saveToLocal(cards){
+    localStorage.setItem('cards', JSON.stringify(cards))
+  }
+
+  deleteFromLocal(cards){
+    cards.splice(index, 1)
+    this.saveToLocal(cards)
+  }
+
+  updateQuality(card, update){
+    this.quality = update;
+    this.saveToLocal(cards)
+  }
+}
